0ce649c2e13489107a5869d555e248ab35ea84b9
softvar <varun2902@gmail.com>
2013-09-17 00:42:15 +0530

    init

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template("my-form.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+if __name__ == '__main__':
+    app.run()
