3d65b32bec63729cabc77b350cdf6b774d51a836
softvar <varun2902@gmail.com>
2013-09-18 21:53:24 +0530

    json2html

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-import os
+
+from flask import json
+import ordereddict
+import HTMLParser
 from flask import Flask
 from flask import request
 from flask import render_template
 
 app = Flask(__name__)
+
+a = ''	
 
 @app.route('/')
 def my_form():
@@ -13,6 +18,70 @@ def my_form():
 def my_form_post():
 
     text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    checkbox = request.form['users']
+    style=""
+    print checkbox
+    if(checkbox=="1"):
+    	style="<table class=\"table table-condensed table-bordered table-hover\""
+    else:
+    	style="<table border=\"1\""
+    #text = {'a':'1','b':'2'}
+    jso = '''{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}'''
+    #json_input = json.dumps(text)
+    try:
+        ordered_json = json.loads(text, object_pairs_hook=ordereddict.OrderedDict)
+    	#print ordered_json
+        processed_text = htmlConvertor(ordered_json,style)
+    	#rep(jso)
+        global a
+        a= ''
+        html_parser = HTMLParser.HTMLParser()
+        return render_template("my-form.html",	processed_text=html_parser.unescape(processed_text),pro = text)
+    except:
+    	return render_template("my-form.html",error="Error Parsing JSON!")
 
+def htmlConvertor(ordered_json,style):
+		global a
+		a=a+ style + "<tr>"
+		for k,v in ordered_json.iteritems():
+			a=a+ '<th>'+ str(k) +'</th>'
+			if(isinstance(v,list)):
+				a=a+ '<td><ul>'
+				for i in range(0,len(v)):
+					a=a+ '<li>'+str(v[i])+'</li>'
+				a=a+ '</ul></td>'
+				a=a+ '</tr>'
+			elif(isinstance(v,unicode)):
+				a=a+ '<td>'+ str(v) +'</td>'
+				a=a+ '</tr>'
+			else:
+				a=a+ '<td>'
+				a=a+ '<table border="1">'
+				htmlConvertor(v,style)
+				a=a+ '</table></td>'
+		a=a+ '</tr></table>'
+		return a
+
+if __name__ == '__main__':
+    app.run(debug = True)
---
+++ static/convert.js
@@ -0,0 +1,48 @@
+
+
+function process() {
+        document.getElementById('table').style.display = "none";
+        
+    }
+
+processed_data = document.getElementById('table').innerHTML;
+    	if(processed_data){
+    		convertHtmlToText();
+    	}
+    	function convertHtmlToText() {
+    var inputText = document.getElementById("table").innerHTML;
+    var returnText = "" + inputText;
+
+    //-- remove BR tags and replace them with line break
+    returnText=returnText.replace(/<br>/gi, "\n");
+    returnText=returnText.replace(/<br\s\/>/gi, "\n");
+    returnText=returnText.replace(/<br\/>/gi, "\n");
+
+    //-- remove P and A tags but preserve what's inside of them
+    returnText=returnText.replace(/<p.*>/gi, "\n");
+    returnText=returnText.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, " $2 ($1)");
+
+    //-- remove all inside SCRIPT and STYLE tags
+    returnText=returnText.replace(/<script.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/script>/gi, "");
+    returnText=returnText.replace(/<style.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/style>/gi, "");
+    //-- remove all else
+    returnText=returnText.replace(/<(?:.|\s)*?>/g, "");
+
+    //-- get rid of more than 2 multiple line breaks:
+    returnText=returnText.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/gim, "\n\n");
+
+    //-- get rid of more than 2 spaces:
+    returnText = returnText.replace(/ +(?= )/g,'');
+
+    //-- get rid of html-encoded characters:
+    returnText=returnText.replace(/&nbsp;/gi," ");
+    returnText=returnText.replace(/&amp;/gi,"&");
+    returnText=returnText.replace(/&quot;/gi,'"');
+    returnText=returnText.replace(/&lt;/gi,'<');
+    returnText=returnText.replace(/&gt;/gi,'>');
+
+    //-- return
+    document.getElementById("outputTable").innerHTML = returnText;
+    if(returnText!=null)
+    document.getElementById("output").value = returnText;
+    }
