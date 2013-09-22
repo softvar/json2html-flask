83bdab8750f2f6dea94f96397844893fd44caee8
softvar <varun2902@gmail.com>
2013-09-22 18:45:32 +0530

    strict and flexible JSON :)

--- hello.py
+++ hello.py
@@ -46,6 +46,7 @@ def my_form_post():
     #json_input = json.dumps(text)
     try:
         ordered_json = json.loads(text, object_pairs_hook=ordereddict.OrderedDict)
+        #print ordered_json
         processed_text = htmlConvertor(ordered_json,style)
 
         html_parser = HTMLParser.HTMLParser()
@@ -53,44 +54,60 @@ def my_form_post():
         a = ''
         return render_template("my-form.html",	processed_text=html_parser.unescape(processed_text),pro = text)
     except:
-        return render_template("my-form.html",error="Error Parsing JSON!")
+        raise
+        #return render_template("my-form.html",error="Error Parsing JSON!")
 
+def iterJson(ordered_json,style):
+	global a
+	a=a+ style 
+	for k,v in ordered_json.iteritems():
+		a=a+ '<tr>'
+		a=a+ '<th>'+ str(k) +'</th>'
+		if(isinstance(v,list)):
+			a=a+ '<td><ul>'
+			for i in range(0,len(v)):
+				if(isinstance(v[i],unicode)):
+					a=a+ '<li>'+str(v[i])+'</li>'
+				elif(isinstance(v[i],int)):
+					a=a+ '<li>'+str(v[i])+'</li>'
+				elif(isinstance(v[i],list)==False):
+					iterJson(v[i],style)
+			a=a+ '</ul></td>'
+			a=a+ '</tr>'
+		elif(isinstance(v,unicode)):
+			a=a+ '<td>'+ str(v) +'</td>'
+			a=a+ '</tr>'
+		elif(isinstance(v,int) or isinstance(v,float)):
+			a=a+ '<td>'+ str(v) +'</td>'
+			a=a+ '</tr>'
+		else:
+			a=a+ '<td>'
+			#a=a+ '<table border="1">'
+			iterJson(v,style)
+			a=a+ '</td></tr>'
+	a=a+ '</table>'
 
 def htmlConvertor(ordered_json,style):
-		'''
-		converts JSON Object into human readable HTML representation
-		generating HTML table code with raw/bootstrap styling.
-		'''
-		global a
-		a=a+ style 
+	'''
+	converts JSON Object into human readable HTML representation
+	generating HTML table code with raw/bootstrap styling.
+	'''
+	global a
+	try:
 		for k,v in ordered_json.iteritems():
-			a=a+ '<tr>'
-			a=a+ '<th>'+ str(k) +'</th>'
-			if(isinstance(v,list)):
-				a=a+ '<td><ul>'
-				for i in range(0,len(v)):
-					if(isinstance(v[i],unicode)):
-						a=a+ '<li>'+str(v[i])+'</li>'
-					elif(isinstance(v[i],int)):
-						a=a+ '<li>'+str(v[i])+'</li>'
-					elif(isinstance(v[i],list)==False):
-						htmlConvertor(v[i],style)
-				a=a+ '</ul></td>'
-				a=a+ '</tr>'
-			elif(isinstance(v,unicode)):
-				a=a+ '<td>'+ str(v) +'</td>'
-				a=a+ '</tr>'
-			elif(isinstance(v,int)):
-				a=a+ '<td>'+ str(v) +'</td>'
-				a=a+ '</tr>'
-			else:
-				a=a+ '<td>'
-				#a=a+ '<table border="1">'
-				htmlConvertor(v,style)
-				a=a+ '</td></tr>'
-				#a=a+ '</table></td>'
-		a=a+ '</table>'
-		return a
+			pass
+		iterJson(ordered_json,style)
+		
+	except:
+		for i in range(0,len(ordered_json)):
+			if(isinstance(ordered_json[i],unicode)):
+				a=a+ '<li>'+str(ordered_json[i])+'</li>'
+			elif(isinstance(ordered_json[i],int)):
+				a=a+ '<li>'+str(ordered_json[i])+'</li>'
+			elif(isinstance(ordered_json[i],list)==False):
+				htmlConvertor(ordered_json[i],style)
+
+	return a
 
 if __name__ == '__main__':
     app.run(debug = True)
